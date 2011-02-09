221937f65ac7ab37795f092edf8c19ac11eb5f0a
Jason Weaver <indyplanets@gmail.com>
2011-02-09 14:21:44 -0600

    Moved functions back to functions.php file

--- functions.php
+++ functions.php
@@ -1,9 +1,447 @@
 <?php
 /**
+ * county_ext functions and definitions
  *
  * @package WordPress
  * @subpackage county_ext
  * @since county_ext 1.0
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
  *
- **/
-require_once(TEMPLATEPATH.'/lib/init.php');
+ * Used to set the width of images and content. Should be equal to the width the theme
+ * is designed for, generally via the style.css stylesheet.
+ */
+if ( ! isset( $content_width ) )
+	$content_width = 640;
+
+/** Tell WordPress to run county_ext_setup() when the 'after_setup_theme' hook is run. */
+add_action( 'after_setup_theme', 'county_ext_setup' );
+
+if ( ! function_exists( 'county_ext_setup' ) ):
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * To override county_ext_setup() in a child theme, add your own county_ext_setup to your child theme's
+ * functions.php file.
+ *
+ * @uses add_theme_support() To add support for post thumbnails and automatic feed links.
+ * @uses register_nav_menus() To add support for navigation menus.
+ * @uses add_custom_background() To add support for a custom background.
+ * @uses add_editor_style() To style the visual editor.
+ * @uses load_theme_textdomain() For translation/localization support.
+ * @uses set_post_thumbnail_size() To set a custom post thumbnail size.
+ *
+ * @since county_ext 1.0
+ */
+function county_ext_setup() {
+
+	// This theme styles the visual editor with editor-style.css to match the theme style.
+	add_editor_style();
+
+	// This theme uses post thumbnails
+	add_theme_support( 'post-thumbnails' );
+	// Add new image sizes
+	add_image_size('featured',960,9999);
+	// Add default posts and comments RSS feed links to head
+	add_theme_support( 'automatic-feed-links' );
+
+	// Make theme available for translation
+	// Translations can be filed in the /languages/ directory
+	load_theme_textdomain( 'county_ext', TEMPLATEPATH . '/languages' );
+
+	$locale = get_locale();
+	$locale_file = TEMPLATEPATH . "/languages/$locale.php";
+	if ( is_readable( $locale_file ) )
+		require_once( $locale_file );
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'primary' => __( 'Primary Navigation', 'county_ext' ),
+	) );
+
+	// This theme allows users to set a custom background
+	add_custom_background();
+	
+
+		//Add jquery to theme
+		function load_tabs_js() {
+		        // instruction to only load if it is not the admin area
+			if ( !is_admin() ) { 
+			// register script location with wp_register_script	
+		   	wp_register_script('my_jquery',
+		       	get_bloginfo('stylesheet_directory') . '/js/my_jquery.js', array('jquery-ui-tabs'), '1.0');	
+		       // enqueue the custom jquery js
+		   	wp_enqueue_script('my_jquery');	
+			}	         
+		}    
+		add_action('init', 'load_tabs_js');
+}	
+endif;
+
+/**
+**
+** Add extra fields for the featured homepage section
+**
+**/
+/* Define the custom box */
+
+// WP 3.0+
+add_action('add_meta_boxes', 'create_featured_post_meta_box');
+
+// backwards compatible
+// add_action('admin_init', 'create_featured_post_meta_box', 1);
+
+/* Do something with the data entered */
+add_action('save_post', 'save_feature_homapage_postdata');
+
+/* Adds a box to the main column on the Post and Page edit screens */
+function create_featured_post_meta_box() {
+    add_meta_box( 'feature-homepage', __( 'Featured on Homepage?', 'feature-homepage' ), 
+                'feature_homepage_inner_custom_box', 'post', 'side' );
+    add_meta_box( 'feature-homepage', __( 'Featured on Homepage?', 'feature-homepage' ), 
+                'feature_homepage_inner_custom_box', 'page', 'side' );
+}
+
+/* Prints the box content */
+function feature_homepage_inner_custom_box() {
+
+  // Use nonce for verification
+  wp_nonce_field( plugin_basename(__FILE__), 'myplugin_noncename' );
+
+  // The actual fields for data entry
+  echo '<label for="feature-homapage">' . __("feature", 'feature-homepage' ) . '</label> ';
+  echo '<input class="checkbox" type="checkbox" id="feature-home" name="feature-home" value="true">';
+}
+
+/* When the post is saved, saves our custom data */
+function save_feature_homapage_postdata( $post_id ) {
+
+  // verify this came from the our screen and with proper authorization,
+  // because save_post can be triggered at other times
+
+  if ( !wp_verify_nonce( $_POST['myplugin_noncename'], plugin_basename(__FILE__) )) {
+    return $post_id;
+  }
+
+  // verify if this is an auto save routine. If it is our form has not been submitted, so we dont want
+  // to do anything
+  if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) 
+    return $post_id;
+
+  
+  // Check permissions
+  if ( 'page' == $_POST['post_type'] ) {
+    if ( !current_user_can( 'edit_page', $post_id ) )
+      return $post_id;
+  } else {
+    if ( !current_user_can( 'edit_post', $post_id ) )
+      return $post_id;
+  }
+
+  // OK, we're authenticated: we need to find and save the data
+
+  $mydata = $_POST['feature-homepage'];
+
+  // Do something with $mydata 
+
+
+   return $mydata;
+}
+
+/**
+ * Makes some changes to the <title> tag, by filtering the output of wp_title().
+ *
+ * If we have a site description and we're viewing the home page or a blog posts
+ * page (when using a static front page), then we will add the site description.
+ *
+ * If we're viewing a search result, then we're going to recreate the title entirely.
+ * We're going to add page numbers to all titles as well, to the middle of a search
+ * result title and the end of all other titles.
+ *
+ * The site title also gets added to all titles.
+ *
+ * @since county_ext 1.0
+ *
+ * @param string $title Title generated by wp_title()
+ * @param string $separator The separator passed to wp_title(). Twenty Ten uses a
+ * 	vertical bar, "|", as a separator in header.php.
+ * @return string The new title, ready for the <title> tag.
+ */
+function county_ext_filter_wp_title( $title, $separator ) {
+	// Don't affect wp_title() calls in feeds.
+	if ( is_feed() )
+		return $title;
+
+	// The $paged global variable contains the page number of a listing of posts.
+	// The $page global variable contains the page number of a single post that is paged.
+	// We'll display whichever one applies, if we're not looking at the first page.
+	global $paged, $page;
+
+	if ( is_search() ) {
+		// If we're a search, let's start over:
+		$title = sprintf( __( 'Search results for %s', 'county_ext' ), '"' . get_search_query() . '"' );
+		// Add a page number if we're on page 2 or more:
+		if ( $paged >= 2 )
+			$title .= " $separator " . sprintf( __( 'Page %s', 'county_ext' ), $paged );
+		// Add the site name to the end:
+		$title .= " $separator " . get_bloginfo( 'name', 'display' );
+		// We're done. Let's send the new title back to wp_title():
+		return $title;
+	}
+
+	// Otherwise, let's start by adding the site name to the end:
+	$title .= get_bloginfo( 'name', 'display' );
+
+	// If we have a site description and we're on the home/front page, add the description:
+	$site_description = get_bloginfo( 'description', 'display' );
+	if ( $site_description && ( is_home() || is_front_page() ) )
+		$title .= " $separator " . $site_description;
+
+	// Add a page number if necessary:
+	if ( $paged >= 2 || $page >= 2 )
+		$title .= " $separator " . sprintf( __( 'Page %s', 'county_ext' ), max( $paged, $page ) );
+
+	// Return the new title to wp_title():
+	return $title;
+}
+add_filter( 'wp_title', 'county_ext_filter_wp_title', 10, 2 );
+
+/**
+ * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
+ *
+ * To override this in a child theme, remove the filter and optionally add
+ * your own function tied to the wp_page_menu_args filter hook.
+ *
+ * @since county_ext 1.0
+ */
+function county_ext_page_menu_args( $args ) {
+	$args['show_home'] = true;
+	return $args;
+}
+add_filter( 'wp_page_menu_args', 'county_ext_page_menu_args' );
+
+/**
+ * Sets the post excerpt length to 40 characters.
+ *
+ * To override this length in a child theme, remove the filter and add your own
+ * function tied to the excerpt_length filter hook.
+ *
+ * @since county_ext 1.0
+ * @return int
+ */
+function county_ext_excerpt_length( $length ) {
+	return 40;
+}
+add_filter( 'excerpt_length', 'county_ext_excerpt_length' );
+
+/**
+ * Returns a "Continue Reading" link for excerpts
+ *
+ * @since county_ext 1.0
+ * @return string "Continue Reading" link
+ */
+function county_ext_continue_reading_link() {
+	return ' <a href="'. get_permalink() . '">' . __( 'Continue <span class="meta-nav">&rarr;</span>', 'county_ext' ) . '</a>';
+}
+
+/**
+ * Replaces "[...]" (appended to automatically generated excerpts) with an ellipsis and county_ext_continue_reading_link().
+ *
+ * To override this in a child theme, remove the filter and add your own
+ * function tied to the excerpt_more filter hook.
+ *
+ * @since county_ext 1.0
+ * @return string An ellipsis
+ */
+function county_ext_auto_excerpt_more( $more ) {
+	return ' &hellip;' . county_ext_continue_reading_link();
+}
+add_filter( 'excerpt_more', 'county_ext_auto_excerpt_more' );
+
+/**
+ * Adds a pretty "Continue Reading" link to custom post excerpts.
+ *
+ * To override this link in a child theme, remove the filter and add your own
+ * function tied to the get_the_excerpt filter hook.
+ *
+ * @since county_ext 1.0
+ * @return string Excerpt with a pretty "Continue Reading" link
+ */
+function county_ext_custom_excerpt_more( $output ) {
+	if ( has_excerpt() && ! is_attachment() ) {
+		$output .= county_ext_continue_reading_link();
+	}
+	return $output;
+}
+add_filter( 'get_the_excerpt', 'county_ext_custom_excerpt_more' );
+
+/**
+ * Remove inline styles printed when the gallery shortcode is used.
+ *
+ * Galleries are styled by the theme in Twenty Ten's style.css.
+ *
+ * @since county_ext 1.0
+ * @return string The gallery style filter, with the styles themselves removed.
+ */
+function county_ext_remove_gallery_css( $css ) {
+	return preg_replace( "#<style type='text/css'>(.*?)</style>#s", '', $css );
+}
+add_filter( 'gallery_style', 'county_ext_remove_gallery_css' );
+
+if ( ! function_exists( 'county_ext_comment' ) ) :
+/**
+ * Template for comments and pingbacks.
+ *
+ * To override this walker in a child theme without modifying the comments template
+ * simply create your own county_ext_comment(), and that function will be used instead.
+ *
+ * Used as a callback by wp_list_comments() for displaying the comments.
+ *
+ * @since county_ext 1.0
+ */
+function county_ext_comment( $comment, $args, $depth ) {
+	$GLOBALS['comment'] = $comment;
+	switch ( $comment->comment_type ) :
+		case '' :
+	?>
+	<li <?php comment_class(); ?> id="li-comment-<?php comment_ID(); ?>">
+		<div id="comment-<?php comment_ID(); ?>">
+		<div class="comment-author vcard">
+			<?php echo get_avatar( $comment, 40 ); ?>
+			<?php printf( __( '%s <span class="says">says:</span>', 'county_ext' ), sprintf( '<cite class="fn">%s</cite>', get_comment_author_link() ) ); ?>
+		</div><!-- .comment-author .vcard -->
+		<?php if ( $comment->comment_approved == '0' ) : ?>
+			<em><?php _e( 'Your comment is awaiting moderation.', 'county_ext' ); ?></em>
+			<br />
+		<?php endif; ?>
+
+		<div class="comment-meta commentmetadata"><a href="<?php echo esc_url( get_comment_link( $comment->comment_ID ) ); ?>">
+			<?php
+				/* translators: 1: date, 2: time */
+				printf( __( '%1$s at %2$s', 'county_ext' ), get_comment_date(),  get_comment_time() ); ?></a><?php edit_comment_link( __( '(Edit)', 'county_ext' ), ' ' );
+			?>
+		</div><!-- .comment-meta .commentmetadata -->
+
+		<div class="comment-body"><?php comment_text(); ?></div>
+
+		<div class="reply">
+			<?php comment_reply_link( array_merge( $args, array( 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
+		</div><!-- .reply -->
+	</div><!-- #comment-##  -->
+
+	<?php
+			break;
+		case 'pingback'  :
+		case 'trackback' :
+	?>
+	<li class="post pingback">
+		<p><?php _e( 'Pingback:', 'county_ext' ); ?> <?php comment_author_link(); ?><?php edit_comment_link( __('(Edit)', 'county_ext'), ' ' ); ?></p>
+	<?php
+			break;
+	endswitch;
+}
+endif;
+
+/**
+ * Register widgetized areas, including two sidebars and four widget-ready areas in the sidebar.
+ *
+ * To override county_ext_widgets_init() in a child theme, remove the action hook and add your own
+ * function tied to the init hook.
+ *
+ * @uses register_sidebar
+ */
+function county_ext_widgets_init() {
+	// Area 1, located at the top of the sidebar.
+	register_sidebar( array(
+		'name' => __( 'Primary Widget Area', 'county_ext' ),
+		'id' => 'primary-widget-area',
+		'description' => __( 'The primary widget area', 'county_ext' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+
+	// Area 3, located in the second sidebar.
+	register_sidebar( array(
+		'name' => __( 'Aside Widget Area', 'county_ext' ),
+		'id' => 'aside-widget-area',
+		'description' => __( 'The aside widget area', 'county_ext' ),
+		'before_widget' => '<li id="%1$s" class="widget-container %2$s">',
+		'after_widget' => '</li>',
+		'before_title' => '<h3 class="widget-title">',
+		'after_title' => '</h3>',
+	) );
+
+}
+/** Register sidebars by running county_ext_widgets_init() on the widgets_init hook. */
+add_action( 'widgets_init', 'county_ext_widgets_init' );
+
+/**
+ * Removes the default styles that are packaged with the Recent Comments widget.
+ *
+ * To override this in a child theme, remove the filter and optionally add your own
+ * function tied to the widgets_init action hook.
+ *
+ */
+function county_ext_remove_recent_comments_style() {
+	global $wp_widget_factory;
+	remove_action( 'wp_head', array( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ) );
+}
+add_action( 'widgets_init', 'county_ext_remove_recent_comments_style' );
+
+if ( ! function_exists( 'county_ext_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post—date/time and author.
+ *
+ * @since county_ext 1.0
+ */
+function county_ext_posted_on() {
+	printf( __( '<span class="%1$s">Posted on</span> %2$s <span class="meta-sep">by</span> %3$s', 'county_ext' ),
+		'meta-prep meta-prep-author',
+		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark"><span class="entry-date">%3$s</span></a>',
+			get_permalink(),
+			esc_attr( get_the_time() ),
+			get_the_date()
+		),
+		sprintf( '<span class="author vcard"><a class="url fn n" href="%1$s" title="%2$s">%3$s</a></span>',
+			get_author_posts_url( get_the_author_meta( 'ID' ) ),
+			sprintf( esc_attr__( 'View all posts by %s', 'county_ext' ), get_the_author() ),
+			get_the_author()
+		)
+	);
+}
+endif;
+
+if ( ! function_exists( 'county_ext_posted_in' ) ) :
+/**
+ * Prints HTML with meta information for the current post (category, tags and permalink).
+ *
+ */
+function county_ext_posted_in() {
+	// Retrieves tag list of current post, separated by commas.
+	$tag_list = get_the_tag_list( '', ', ' );
+	if ( $tag_list ) {
+		$posted_in = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'county_ext' );
+	} elseif ( is_object_in_taxonomy( get_post_type(), 'category' ) ) {
+		$posted_in = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'county_ext' );
+	} else {
+		$posted_in = __( 'Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'county_ext' );
+	}
+	// Prints the string, replacing the placeholders.
+	printf(
+		$posted_in,
+		get_the_category_list( ', ' ),
+		$tag_list,
+		get_permalink(),
+		the_title_attribute( 'echo=0' )
+	);
+}
+endif;
