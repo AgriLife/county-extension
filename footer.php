d5aa91d60170a6bf1c749e20f427285f8baae56e
Travis Ward <travis@travisward.com>
2011-05-05 10:06:47 -0500

    added some more footer links

--- footer.php
+++ footer.php
@@ -25,17 +25,15 @@
 		<div id="popular-links">
 			<div class="popular-links">			
 				<h4>Popular Links</h4>
-				<a href=""><img src="<?php bloginfo( 'template_directory' ); ?>/images/agrilife_ext_logo.png" alt="AgriLife Extension video" /></a>	
+				<a href="http://agrilifeextension.tamu.edu/"><img src="<?php bloginfo( 'template_directory' ); ?>/images/agrilife_ext_logo.png" alt="Texas AgriLife Extension" /></a>	
 				<ul>
-					<li><a href="">County Extension Offices</a></li>
-					<li><a href="">Research and Extension 
-					Centers</a></li>
-					<li><a href="">Contact Directory</a></li>
-					<li><a href="">Media Contacts</a></li>					
-					<li><a href="">Corporate Relations</a></li>
-					<li><a href="">Texas 4-H and Youth Dev.</a></li>					
-					<li><a href="">Strategic Plans, Impacts and Roadmaps</a></li>
-					<li class="last"><a href="">Employment Opportunities</a></li>										
+					<li><a href="http://county-tx.tamu.edu/">County Extension Offices</a></li>
+					<li><a href="http://agrilife.tamu.edu/locations-window/#centers">Research and Extension Centers</a></li>
+					<li><a href="http://agdirectory.tamu.edu/">Contact Directory</a></li>
+					<li><a href="http://agrilife.org/today/contact-us/">Media Contacts</a></li>					
+					<li><a href="http://texas4-h.tamu.edu/">Texas 4-H and Youth Dev.</a></li>					
+					<li><a href="http://agrilifeextension.tamu.edu/about/strategyimpact/index.php">Strategic Plans, Impacts and Roadmaps</a></li>
+					<li class="last"><a href="http://agrilifeextension.tamu.edu/careers/index.php">Employment Opportunities</a></li>										
 				</ul>		
 			</div><!-- .popular-links -->			
 		</div><!-- #popular-links -->
@@ -43,20 +41,19 @@
 		<div id="texas-a-m">
 			<div class="texas-a-m">			
 			<h4>Required Links</h4>
-				<a href=""><img src="<?php bloginfo( 'template_directory' ); ?>/images/texas-a-m-system.png" alt="Texas A&amp;M System image" /></a>	
+				<a href="http://www.tamus.edu"><img src="<?php bloginfo( 'template_directory' ); ?>/images/texas-a-m-system.png" alt="Texas A&amp;M System image" /></a>
 				<ul>
 					<li><a href="http://agrilife.tamu.edu/compact/">Compact with Texans</a></li>
 					<li><a href="http://agrilife.tamu.edu/privacy/">Privacy and Security</a></li>
 					<li><a href="http://itaccessibility.tamu.edu/">Accessibility Policy</a></li>
 					<li><a href="http://www.dir.state.tx.us/standards/link_policy.htm">State Link Policy</a></li>					
-					<li><a href="http://www.tsl.state.tx.us/trail">Statewide Search</a></li>
-					<li><a href="">Plug-ins</a></li>					
-					<li><a href="">Equal Opportunity for Educational Programs Statement</a></li>
-					<li><a href="">Veterans Benefits</a></li>		
-					<li><a href="">Military Families</a></li>
-					<li><a href="">Risk, Fraud &amp; Misconduct Hotline</a></li>					
-					<li><a href="">Texas Homeland Security</a></li>
-					<li class="last"><a href="">Open Records/Public Information</a></li>																		
+					<li><a href="http://www.tsl.state.tx.us/trail">Statewide Search</a></li>					
+					<li><a href="http://aghr.tamu.edu/education-civil-rights.htm">Equal Opportunity for Educational Programs Statement</a></li>
+					<li><a href="http://www.tamus.edu/veterans/">Veterans Benefits</a></li>		
+					<li><a href="http://fcs.tamu.edu/families/military_families/">Military Families</a></li>
+					<li><a href="https://secure.ethicspoint.com/domain/en/report_custom.asp?clientid=19681">Risk, Fraud &amp; Misconduct Hotline</a></li>
+					<li><a href="http://www.texashomelandsecurity.com/">Texas Homeland Security</a></li>
+					<li class="last"><a href="http://agrilife.tamu.edu/orpi/">Open Records/Public Information</a></li>
 				</ul>		
 			</div><!-- .texas-a-m -->			
 		</div><!-- #texas-a-m -->				
@@ -93,7 +90,7 @@
 		<div id="agrilife-bookstore">
 			<div class="agrilife-bookstore">			
 			<h4>AgriLife Bookstore</h4>
-				<a href=""><img src="<?php bloginfo( 'template_directory' ); ?>/images/bookstore-books.png" alt="Image of books for the AgriLife Bookstore" /></a>
+				<a href="https://agrilifebookstore.org/"><img src="<?php bloginfo( 'template_directory' ); ?>/images/bookstore-books.png" alt="Image of books for the AgriLife Bookstore" /></a>
 				<p>The Texas AgriLife Extension Bookstore offers educational publications, CDs and videos on topics related to agriculture, 4-H and youth development.</p>				
 			</div><!-- .agrilife-bookstore -->			
 		</div><!-- #agrilife-bookstore -->
